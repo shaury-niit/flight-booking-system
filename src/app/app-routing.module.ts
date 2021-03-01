a8338a0556841c9baa3b7cd34fe11ac12734c16a
Shaury Prakash Saxena <Shaury.Saxena@COFORGETECH.COM>
2021-03-01 13:15:37 +0530

    wireframes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+import { FlightsListComponent } from './flights-list/flights-list.component';
+import { BookFlightComponent } from './book-flight/book-flight.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+  RouterModule.forRoot([    
+    {path: '', component: LoginComponent},
+    {path: 'search', component: SearchComponent},
+    {path: 'flightsList', component: FlightsListComponent},
+    {path: 'bookFlight', component: BookFlightComponent}    
+  ]),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+import { FlightsListComponent } from './flights-list/flights-list.component';
+import { BookFlightComponent } from './book-flight/book-flight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SearchComponent,
+    FlightsListComponent,
+    BookFlightComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
